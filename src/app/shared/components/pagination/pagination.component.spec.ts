892ed151ca5a2be190540401b12a776e00fca19e
Giorgos Brantzos <g.brantzos@outlook.com>
2025-01-25 21:48:32 +0200

    Improve tests (parameterized)

--- src/app/shared/components/pagination/pagination.component.spec.ts
+++ src/app/shared/components/pagination/pagination.component.spec.ts
@@ -1,7 +1,65 @@
 import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { EMPTY_PAGING, PaginationComponent, PagingInfo } from './pagination.component';
+import { EMPTY_PAGING, PaginationComponent } from './pagination.component';
+
+const testCases = [
+  {
+    description: 'normal case, first page',
+    pagingInfo: {
+      totalRows: 256,
+      pageSize: 50,
+      current: 1
+    },
+    summaryText: 'Showing 1-50 of 256 rows',
+    rowsPerPageText: '50 rows per page',
+    arrowsState: { first: true, prev: true, next: false, last: false }
+  },
+  {
+    description: 'normal case, second page',
+    pagingInfo: {
+      totalRows: 4654,
+      pageSize: 30,
+      current: 3
+    },
+    summaryText: 'Showing 61-90 of 4654 rows',
+    rowsPerPageText: '30 rows per page',
+    arrowsState: { first: false, prev: false, next: false, last: false }
+  },
+  {
+    description: 'normal case, last page',
+    pagingInfo: {
+      totalRows: 256,
+      pageSize: 50,
+      current: 6
+    },
+    summaryText: 'Showing 251-256 of 256 rows',
+    rowsPerPageText: '50 rows per page',
+    arrowsState: { first: false, prev: false, next: true, last: true }
+  },
+  {
+    description: 'results less than page size',
+    pagingInfo: {
+      totalRows: 26,
+      pageSize: 50,
+      current: 1
+    },
+    summaryText: 'Showing 1-26 rows',
+    rowsPerPageText: '50 rows per page',
+    arrowsState: { first: true, prev: true, next: true, last: true }
+  },
+  {
+    description: 'invalid current page, silently convert to page 1',
+    pagingInfo: {
+      totalRows: 46,
+      pageSize: 70,
+      current: 12
+    },
+    summaryText: 'Showing 1-46 rows',
+    rowsPerPageText: '70 rows per page',
+    arrowsState: { first: true, prev: true, next: true, last: true }
+  }
+];
 
 describe('PaginationComponent', () => {
   async function setup() {
@@ -31,75 +89,24 @@ describe('PaginationComponent', () => {
     expect(summary.nativeElement.innerHTML).toEqual('No rows displayed');
   });
 
-  it('should show correct summary and arrows', async () => {
-    const { debugElement, fixture } = await setup();
-    const summary = debugElement.query(By.css('#summary'));
-    const pageSize = debugElement.query(By.css('#pageSize'));
-    const arFirst = debugElement.query(By.css('#first'));
-    const arPrev = debugElement.query(By.css('#prev'));
-    const arNext = debugElement.query(By.css('#next'));
-    const arLast = debugElement.query(By.css('#last'));
+  testCases.forEach(({ pagingInfo, summaryText, rowsPerPageText, arrowsState, description }) => {
+    it(`should show correct summary and arrows - ${description}`, async () => {
+      const { debugElement, fixture } = await setup();
+      const summary = debugElement.query(By.css('#summary'));
+      const pageSize = debugElement.query(By.css('#pageSize'));
+      const arrowFirst = debugElement.query(By.css('#first'));
+      const arrowPrev = debugElement.query(By.css('#prev'));
+      const arrowNext = debugElement.query(By.css('#next'));
+      const arrowLast = debugElement.query(By.css('#last'));
 
-    // Normal case, first page
-    fixture.componentRef.setInput('pagingInfo', {
-      totalRows: 256,
-      pageSize: 50,
-      current: 1
-    } as PagingInfo);
-    fixture.detectChanges();
-    expect(summary.nativeElement.innerHTML).toEqual('Showing 1-50 of 256 rows');
-    expect(pageSize.nativeElement.innerHTML).toEqual('50 rows per page');
-    expect(arFirst.nativeElement.classList).toContain('disabled');
-    expect(arPrev.nativeElement.classList).toContain('disabled');
-    expect(arNext.nativeElement.classList).not.toContain('disabled');
-    expect(arLast.nativeElement.classList).not.toContain('disabled');
-
-    // Normal case, second page
-    fixture.componentRef.setInput('pagingInfo', {
-      totalRows: 4654,
-      pageSize: 30,
-      current: 3
-    } as PagingInfo);
-    fixture.detectChanges();
-    expect(summary.nativeElement.innerHTML).toEqual('Showing 61-90 of 4654 rows');
-    expect(pageSize.nativeElement.innerHTML).toEqual('30 rows per page');
-    expect(arFirst.nativeElement.classList).not.toContain('disabled');
-    expect(arPrev.nativeElement.classList).not.toContain('disabled');
-    expect(arNext.nativeElement.classList).not.toContain('disabled');
-    expect(arLast.nativeElement.classList).not.toContain('disabled');
-
-    // Normal case, last page
-    fixture.componentRef.setInput('pagingInfo', {
-      totalRows: 256,
-      pageSize: 50,
-      current: 6
-    } as PagingInfo);
-    fixture.detectChanges();
-    expect(summary.nativeElement.innerHTML).toEqual('Showing 251-256 of 256 rows');
-    expect(pageSize.nativeElement.innerHTML).toEqual('50 rows per page');
-    expect(arFirst.nativeElement.classList).not.toContain('disabled');
-    expect(arPrev.nativeElement.classList).not.toContain('disabled');
-    expect(arNext.nativeElement.classList).toContain('disabled');
-    expect(arLast.nativeElement.classList).toContain('disabled');
-
-    // Results less than page size
-    fixture.componentRef.setInput('pagingInfo', {
-      totalRows: 26,
-      pageSize: 50,
-      current: 1
-    } as PagingInfo);
-    fixture.detectChanges();
-    expect(summary.nativeElement.innerHTML).toEqual('Showing 1-26 rows');
-    expect(pageSize.nativeElement.innerHTML).toEqual('50 rows per page');
-
-    // Invalid current page, silently convert to page 1
-    fixture.componentRef.setInput('pagingInfo', {
-      totalRows: 46,
-      pageSize: 70,
-      current: 12
-    } as PagingInfo);
-    fixture.detectChanges();
-    expect(summary.nativeElement.innerHTML).toEqual('Showing 1-46 rows');
-    expect(pageSize.nativeElement.innerHTML).toEqual('70 rows per page');
+      fixture.componentRef.setInput('pagingInfo', pagingInfo);
+      fixture.detectChanges();
+      expect(summary.nativeElement.innerHTML).toEqual(summaryText);
+      expect(pageSize.nativeElement.innerHTML).toEqual(rowsPerPageText);
+      expect(arrowFirst.nativeElement.classList.contains('disabled')).toBe(arrowsState.first);
+      expect(arrowPrev.nativeElement.classList.contains('disabled')).toBe(arrowsState.prev);
+      expect(arrowNext.nativeElement.classList.contains('disabled')).toBe(arrowsState.next);
+      expect(arrowLast.nativeElement.classList.contains('disabled')).toBe(arrowsState.last);
+    });
   });
 });
