b80970d9f4b09d3a5eea7b7aeaf6a7527ee0d127
Giorgos Brantzos <g.brantzos@gmail.com>
2025-01-26 12:52:25 +0200

    Sample initialization spinner

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+/* eslint-disable no-magic-numbers */
+import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+
+export function initializeApp() {
+  return (): Promise<void> => {
+    return new Promise<void>(resolve => {
+      // console.log('Initialization complete');
+      setTimeout(() => {
+        resolve();
+      }, 1500); // Simulate a 5-second initialization task
+    });
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAppInitializer(initializeApp())
   ]
 };
